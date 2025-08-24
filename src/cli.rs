8375bf7998247f16644235473557999b85048468
Matt Burke <spraints@gmail.com>
2025-08-24 17:45:48 -0400

    Start a webserver

--- src/cli.rs
+++ src/cli.rs
@@ -6,4 +6,7 @@ use clap::Parser;
 pub struct Cli {
     #[arg(long, short)]
     pub config: PathBuf,
+
+    #[arg(long, short, default_value = "127.0.0.1:3000")]
+    pub addr: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use std::fs;
 
+use axum::{routing::get, Router};
 use clap::Parser;
 use config::Config;
 
 mod cli;
 mod config;
 
-fn main() {
-    let cli::Cli { config } = cli::Cli::parse();
+#[tokio::main]
+async fn main() {
+    let cli::Cli { addr, config } = cli::Cli::parse();
     let f = fs::File::open(&config).unwrap();
     let config: Config = serde_yaml::from_reader(&f).unwrap();
     println!("Hello, world! {config:?}");
+
+    println!("listening on {addr}");
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
