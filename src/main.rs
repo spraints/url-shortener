b918c36057a80438c32c7cc2c4f68957a634bb84
Matt Burke <spraints@gmail.com>
2025-08-24 20:12:34 -0400

    nah, don't actually log the location
    location might have secrets in it, which we don't want to expose.

--- src/main.rs
+++ src/main.rs
@@ -38,11 +38,7 @@ async fn main() {
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
-                .on_response(
-                    DefaultOnResponse::new()
-                        .level(Level::INFO)
-                        .include_headers(true),
-                ),
+                .on_response(DefaultOnResponse::new().level(Level::INFO)),
         )
         .with_state(state);
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
